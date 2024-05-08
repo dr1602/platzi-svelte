699cd2f856fee7d7e7ffcfca1c320ee975e495b9
dr1602 <109833006+dr1602@users.noreply.github.com>
2024-05-08 10:06:04 -0600

    Create PlusButton.svelte

---
+++ PlusButton.svelte
@@ -0,0 +1,27 @@
+	<script>
+		import { button } from './styles.js'
+        export let movieQuantity;
+        export let maxTickets;
+
+		const increment = () => {
+			console.log('Increment button clicked');
+			console.log('Before increment - movieQuantity:', movieQuantity);
+			console.log('Before increment - maxTickets:', maxTickets);
+
+			if(movieQuantity < maxTickets) {
+        		movieQuantity++;			
+			}
+
+			console.log('After increment - movieQuantity:', movieQuantity);
+			console.log('After increment - maxTickets:', maxTickets);
+		}
+    </script>
+    
+    <button 
+		class='{button}'
+		type='button'
+		on:click = {increment}
+		disabled = {movieQuantity >= maxTickets}
+	>
+		+
+	</button>
