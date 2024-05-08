fff2cf71e241078b50548839a4d58f2d4b6636ed
dr1602 <109833006+dr1602@users.noreply.github.com>
2024-05-08 10:04:17 -0600

    Create App.svelte

---
+++ App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Form from "./Form.svelte";
+	import { title } from './styles.js';
+</script>
+
+<main>
+	<h2 class='{title}'> Peliculas </h2>
+	<Form />
+</main>
+
+<style>
+
+main {
+	font-family: sans-serif;
+	text-align: center;
+}
+
+:global(body) {
+	/* --bg: #737574;
+	--color: #f8ff6a;
+	--color2: #a78fff; */
+	background: var(--bg);
+	color: var(--color);
+}
+
+</style>
