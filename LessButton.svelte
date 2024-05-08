7533f71c8a187b0a37c1d68a3776b55a58be907e
dr1602 <109833006+dr1602@users.noreply.github.com>
2024-05-08 10:05:29 -0600

    Create LessButton.svelte

---
+++ LessButton.svelte
@@ -0,0 +1,19 @@
+	<script>
+		import { button } from './styles.js'
+        export let movieQuantity;
+		
+		const decrement = () => {
+			if(movieQuantity > 0) {
+        		movieQuantity -= 1;			
+			}
+		}
+    </script>
+    
+    <button
+		class='{button}'
+		type='button' 
+		on:click = {decrement}
+		disabled = {movieQuantity <= 0}
+	>
+		-
+	</button>
